4c7da29c4dabc6749deb6a9a22ea928d7b8e51a5
alexandrupuscas <alexandru.puscas@clever-software-solutions.com>
2017-06-23 15:32:16 +0300

    Implemented https

--- socket.js
+++ socket.js
@@ -1,15 +1,16 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var https = require('https').Server(app);
+var io = require('socket.io')(https);
 var Redis = require('ioredis');
 var redis = new Redis();
+
 redis.subscribe('test-channel', function(err, count) {
 });
 redis.on('message', function(channel, message) {
-    console.log('Message Recieved: ' + message);
+    console.log('Message Received: ' + message);
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
-http.listen(3000, function(){
+https.listen(3000, function(){
     console.log('Listening on Port 3000');
 });
