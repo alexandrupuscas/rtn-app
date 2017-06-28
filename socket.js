bbd861da180ba4a5cf46525788e10e5a689cf68c
alexandrupuscas <alexandru.puscas@clever-software-solutions.com>
2017-06-28 14:51:49 +0300

    Added socket.io client

--- socket.js
+++ socket.js
@@ -1,36 +1,21 @@
+//--------------HTTP-----------------
 // var app = require('express')();
-// var https = require('https').Server(app);
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 // var Redis = require('ioredis');
 // var redis = new Redis();
-// var fs = require('fs');
-//
-// var options = {
-//     key:    fs.readFileSync('encryption/private.key'),
-//     cert:   fs.readFileSync('encryption/rtn-app.com.csr')
-//     // ca:     fs.readFileSync('encryption/rtn-app.com.crt')
-// };
-//
-//
 // redis.subscribe('test-channel', function(err, count) {
 // });
 // redis.on('message', function(channel, message) {
-//     console.log('Message Recieved: ' + message);
+//     console.log('Message Received: ' + message);
 //     message = JSON.parse(message);
 //     io.emit(channel + ':' + message.event, message.data);
 // });
-//
-//
-// var application = https.createServer(options);
-// io = require('socket.io').listen(application);     //socket.io server listens to https connections
-// application.listen(3000, function(){
-//     console.log('Listening on Port 3000');
-// });
-
 // http.listen(3000, function(){
 //     console.log('Listening on Port 3000');
 // });
 
-
+//---------HTTPS--------------------
 var express = require('express');
 var app = express();
 var fs = require('fs');
@@ -50,7 +35,7 @@ var options = {
 redis.subscribe('test-channel', function(err, count) {
 });
 redis.on('message', function(channel, message) {
-    console.log('Message Recieved: ' + message);
+    console.log('Message Received: ' + message);
     console.log('kakaka', options.cert);
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
