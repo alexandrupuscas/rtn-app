3cfecee8c6d9332199b7bf388535ac22563e6a35
alexandrupuscas <alexandru.puscas@clever-software-solutions.com>
2017-06-28 17:43:49 +0300

    Edited socket client

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.socket.io/socket.io-1.4.5.js"></script>
     <script>
         //var socket = io('http://localhost:3000');
-        var socket = io('http://192.168.10.15:3000');
+        var socket = io('https://rtn-app.herokuapp.com');
         socket.on("test-channel:App\\Events\\EventName", function(message){
             // increase the power everytime we load test route
             $('#power').text(parseInt($('#power').text()) + parseInt(message.data.power));
